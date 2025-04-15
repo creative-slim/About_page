21bc553bfeb77fa07703dfe2d8330b1d23e2c9fc
creative-slim <sa@creative-directors.com>
2025-04-15 16:31:27 +0200

    no bg

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import { Planets } from "./Planets.jsx";
 
 export default function App() {
   return (
-    <Canvas camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 2] }}>
-      <color attach="background" args={["#000000"]} />
+    <Canvas
+      gl={{ alpha: true }}
+      camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 2] }}
+    >
+      {/* <color attach="background" args={["#000000"]} /> */}
       <directionalLight position={[-10, -10, -5]} intensity={0.5} />
       <Planets />
 
